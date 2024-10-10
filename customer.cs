162ada97b71f483d837127c0bc85540f07bf041c
NGUYEN TUAN HOANG <thisishoang1512@gmail.com>
2024-10-10 19:04:59 +1000

    Customer and quotation request draft

---
+++ customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as2
+{
+    public class Customer : user
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int phoneNumber { get; set; }
+        public string companyName { get; set; }
+        public string address { get; set; }
+        public string contry { get; set; }
+
+        public Customer(string username, string password) : base(username, password)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ user.cs
@@ -0,0 +1,36 @@
+﻿using as2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as2
+{
+    public class user
+    {
+        string username;
+        string password;
+        int user_id;
+        enum user_type { staff, customer }
+        public user(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+
+        public bool create_user(string username, string password)
+        {
+            if (username != null && password != null)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public bool delete_user(string user_id)
+        { return false; }
+    }
+}
