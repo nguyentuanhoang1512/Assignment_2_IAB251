eaa9ee5ace88dc62aa7d1779803dedb57dcbe0f6
NGUYEN TUAN HOANG <thisishoang1512@gmail.com>
2024-10-15 13:17:05 +1000

    Employee and main screen

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace IAB251_A2
+﻿using as2;
+
+namespace IAB251_A2
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static List<Customer> customers = new List<Customer>();
+        static List<Employee> employees = new List<Employee>();
+
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            MainScreen();
+        }
+
+        public static void MainScreen()
+        {
+            Console.WriteLine("Welcome to InterportCargo System");
+            Console.WriteLine("1. Register as Customer");
+            Console.WriteLine("2. Register as Employee");
+            Console.WriteLine("3. Log in");
+            Console.WriteLine("4. Submit Quotation Request");
+            Console.WriteLine("5. Exit");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+     //               RegisterCustomer();
+                    break;
+                case "2":
+      //              RegisterEmployee();
+                    break;
+                case "5":
+       //             ExitSystem();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    MainScreen();
+                    break;
+            }
         }
     }
-}
---
+++ employee.cs
@@ -0,0 +1,25 @@
+﻿using as2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as2
+{
+    public class Employee : user
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int phoneNumber { get; set; }
+        public string EmployeeType { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+
+        public Employee(string username, string password) : base(username, password)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
