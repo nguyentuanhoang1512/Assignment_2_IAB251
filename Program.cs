3448978d1d02a068e363f2c3fdc39b43df1348fb
Luke Thomas <n11542829@qut.edu.au>
2024-10-29 17:15:57 +1000

    Main program started

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using IAB251_A2.Models; 
+﻿using IAB251_A2.Models;
+using System.Runtime.CompilerServices;
 
 namespace IAB251_A2
 {
@@ -6,7 +7,46 @@ namespace IAB251_A2
     {
         static void Main(string[] args)
         {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Welcome to Interport Cargo Services!");
+                Console.WriteLine("1. Register");
+                Console.WriteLine("2. Log In");
+                Console.WriteLine("0. Exit");
 
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    //Register Stuff
+                    Console.WriteLine("Select User type");
+                    Console.WriteLine("1. Customer");
+                    Console.WriteLine("2. Employee");
+                    Console.WriteLine("0. Back");
+
+                    int UserTypeInput = int.Parse(Console.ReadLine());
+                    
+                    if (UserTypeInput == 1)
+                    {
+                        //Customer Register
+                    } else if (UserTypeInput == 2)
+                    {
+                        //Employee Register
+                    } 
+
+
+
+
+                } else if (input == 2)
+                {
+                    //Log in stuff
+                } else if (input == 0)
+                {
+                    running = false;
+                }
+                
+            }
         }
     }
 }
